148b6aaae7f946285efa3efa66641051329077d7
MagnuZvn <manuel.lg077@gmail.com>
2023-02-05 21:22:33 -0300

    modificaciones en proyecto

--- escolarSystem.js
+++ escolarSystem.js
@@ -1,26 +1,34 @@
-
-//obtencion de los datos rellenados en el formulario
-let nameStudent = document.getElementById("nameStudent").value;
-let mailStudent = document.getElementById("emailStudent").value;
-let dniStudent = document.getElementById("dniStudent").value;
-let courseStudent = document.querySelector('input[name="course"]:checked').value; //aceso al input de tipo radio marcado
-console.log(courseStudent);
-if (courseStudent) {
-  if (courseStudent == "OTRA") {
-    let inputCourseStudent = document.getElementById("inputCourse").style.visibility = "visible";
-    let textInputCourseStudent = document.getElementById("textInputCourse").style.visibility = "visible";
-    courseStudent = inputCourseStudent.value;
+let formStudent = document.getElementById("formContent");
+formStudent.addEventListener("submit", (submitForm) => {
+  let error = false; //cuando haya un error, no se enviara el formulario de inscripcion
+  //obtencion de los datos rellenados en el formulario
+  let nameStudent = document.getElementById("nameStudent").value;
+  let mailStudent = document.getElementById("emailStudent").value;
+  let dniStudent = document.getElementById("dniStudent").value;
+  //verificacion de la opcion marcada en los radioButtons...
+  let radioButtons = document.querySelector('input[name="course"]:checked'); //verifica si hay elementos marcados
+  if (radioButtons) {
+    var courseStudent;
+    if (radioButtons.checked.value == "OTRA") {
+      let inputCourseStudent = document.getElementById("inputCourse").style.visibility = "visible";
+      document.getElementById("textInputCourse").style.visibility = "visible";
+      courseStudent = inputCourseStudent.value; //definicion de la materia escogida
+    }
+    else { courseStudent = radioButtons.value }
+    alert("DATOS REGISTRADOS\nNOMBRE: " + nameStudent + "\nCORREO ELECTRONICO: " + mailStudent + "\nDNI: " + dniStudent + "\nMATERIA A REGISTRAR: " + courseStudent);
   }
-}
-else {
-  alert("No se ha seleccionado ninguna materia");
-}
-//prevencion de comportamiento por defecto del formulario
-document.getElementById("formContent").addEventListener('submit', (e) => {
-  e.preventDefault();
-});
-function result() {
-  alert("DATOS REGISTRADOS\nNOMBRE:" + nameStudent + "\nCORREO ELECTRONICO: " + mailStudent + "\nDNI: " + dniStudent + "\nMATERIA A REGISTRAR: " + courseStudent);
-}
-submitButton = document.getElementById("buttonStudent");
-submitButton.addEventListener("click", result); 
+  else {
+    alert("No se ha seleccionado ninguna materia");
+    error = true;
+  }
+  if (error) { submitForm.preventDefault(); }
+  else { alert("INSCRIPCION REALIZADA CON EXITO"); }
+
+})
+
+
+
+//submitButton = document.getElementById("buttonStudent");
+//submitButton.addEventListener("click", result);
+//configuracion y envio del formulario 
+
