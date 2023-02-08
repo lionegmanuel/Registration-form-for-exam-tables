f5cf285cb6cab118aa7a48a79bd605ec850109cd
MagnuZvn <lionegmanuel@gmail.com>
2023-02-07 21:46:19 -0300

    mejoras y solucion a la seleccion de radios

--- escolarSystem.js
+++ escolarSystem.js
@@ -8,27 +8,35 @@ formStudent.addEventListener("click", (submitForm) => {
   //verificacion de la opcion marcada en los radioButtons...
   let radioButtons = document.querySelector('input[name="course"]:checked'); //verifica si hay elementos marcados
   if (radioButtons) {
-    var courseStudent;
-    if (radioButtons.checked.value == "OTRA") {
-      let inputCourseStudent = document.getElementById("inputCourse").style.visibility = "visible";
-      document.getElementById("textInputCourse").style.visibility = "visible";
-      courseStudent = inputCourseStudent.value; //definicion de la materia escogida
-    }
-    else { courseStudent = radioButtons.value }
-    alert("DATOS REGISTRADOS\nNOMBRE: " + nameStudent + "\nCORREO ELECTRONICO: " + mailStudent + "\nDNI: " + dniStudent + "\nMATERIA A REGISTRAR: " + courseStudent);
+    //if (radioButtons.value == "Otra") {
+    //let inputCourseStudent = document.getElementById("inputCourse").style.visibility = "visible";
+    //document.getElementById("textInputCourse").style.visibility = "visible";
+    //courseStudent = inputCourseStudent.value; //definicion de la materia escogida
+    //alert("DATOS REGISTRADOS\nNOMBRE: " + nameStudent + "\nCORREO ELECTRONICO: " + mailStudent + "\nDNI: " + dniStudent + "\nMATERIA A REGISTRAR: " + courseStudent);
+    if (radioButtons.value == "Otra") { let courseStudent = addCourse(); alert("DATOS REGISTRADOS\nNOMBRE: " + nameStudent + "\nCORREO ELECTRONICO: " + mailStudent + "\nDNI: " + dniStudent + "\nMATERIA A REGISTRAR: " + courseStudent); }
+
+    else { let courseStudent = radioButtons.value; alert("DATOS REGISTRADOS\nNOMBRE: " + nameStudent + "\nCORREO ELECTRONICO: " + mailStudent + "\nDNI: " + dniStudent + "\nMATERIA A REGISTRAR: " + courseStudent); }
+
   }
   else {
     alert("No se ha seleccionado ninguna materia");
     error = true;
   }
+
   if (error) { let buttonForm = document.getElementById("buttonStudent"); buttonForm.preventDefault(); }
   else { alert("INSCRIPCION REALIZADA CON EXITO"); }
 
 })
 
+function addCourse() {
 
+  alert("Ingrese la materia adeudada a continuacion");
+  let inputCourseStudent = document.getElementById("inputCourse").style.visibility = "visible"; //muestra las opciones para ingresar la materia en el cuadro de texto correspondiente
+  document.getElementById("textInputCourse").style.visibility = "visible";
+  let courseStudent = inputCourseStudent.value;
+  return courseStudent;
 
-//submitButton = document.getElementById("buttonStudent");
-//submitButton.addEventListener("click", result);
-//configuracion y envio del formulario 
+}
+let inputOtherCourse = document.getElementById("radioFour");
+inputOtherCourse.addEventListener("click", addCourse);
 
