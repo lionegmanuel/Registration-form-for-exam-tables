13a8777b73a295a88f753fdf018c28663bea22e7
MagnuZvn <lionegmanuel@gmail.com>
2023-02-09 19:12:56 -0300

    finalizacion de formulario (parte logica) falta terminar responsividad

--- escolarSystem.js
+++ escolarSystem.js
@@ -5,10 +5,6 @@ inputCourse.addEventListener("click", (addCourse) => {
   document.getElementById("inputCourse").style.visibility = "visible"; //muestra las opciones para ingresar la materia en el cuadro de texto correspondiente
   document.getElementById("textInputCourse").style.visibility = "visible";
 });
-
-
-
-
 let formStudent = document.getElementById("buttonStudent");
 formStudent.addEventListener("click", (submitForm) => {
   let error = false; //cuando haya un error, no se enviara el formulario de inscripcion
@@ -17,29 +13,28 @@ formStudent.addEventListener("click", (submitForm) => {
   let mailStudent = document.getElementById("emailStudent").value;
   let dniStudent = document.getElementById("dniStudent").value;
   //verificacion de la opcion marcada en los radioButtons...
+
   let radioButtons = document.querySelector('input[name="course"]:checked'); //verifica si hay elementos marcados
   if (radioButtons) {
     if (radioButtons.value == "Otra") {
       let inputCourseStudent = document.getElementById("inputCourse");
       let courseStudent = inputCourseStudent.value;
       alert("DATOS REGISTRADOS\nNOMBRE: " + nameStudent + "\nCORREO ELECTRONICO: " + mailStudent + "\nDNI: " + dniStudent + "\nMATERIA A REGISTRAR: " + courseStudent);
+      console.log(nameStudent.value);
     }
-
+    else if (nameStudent.length == 0 || mailStudent.length == 0 || dniStudent.length == 0) {
+      alert("¡Complete todos los campos!"); error = true; var userAdvertence = true;
+    }
     else { let courseStudent = radioButtons.value; alert("DATOS REGISTRADOS\nNOMBRE: " + nameStudent + "\nCORREO ELECTRONICO: " + mailStudent + "\nDNI: " + dniStudent + "\nMATERIA A REGISTRAR: " + courseStudent); }
-
   }
   else {
-    alert("No se ha seleccionado ninguna materia");
-    error = true;
+    if (userAdvertence) error = false;
+    else {
+      alert("¡No se ha seleccionado ninguna materia!"); error = true;
+    }
   }
-
-  if (error) { let buttonForm = document.getElementById("buttonStudent"); buttonForm.preventDefault(); }
-  else {
-    alert("INSCRIPCION REALIZADA CON EXITO"); document.getElementById("inputCourse").style.visibility = "hidden"; //oculta las opciones para ingresar la materia en el cuadro de texto correspondiente
-    document.getElementById("textInputCourse").style.visibility = "hidden";
-    document.querySelector(".formExam").style.visibility = "hidden";
-
-  }
+  if (error) submitForm.preventDefault();  //previene el envio del formulario
+  else alert("INSCRIPCION REALIZADA CON EXITO");
 
 })
 
